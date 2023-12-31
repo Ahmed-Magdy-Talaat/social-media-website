60319ec7636aec0e31ed966b7ac0efadd0d7ee3f
Ahmed-Magdy-Talaat <85371544+Ahmed-Magdy-Talaat@users.noreply.github.com>
2023-12-31 21:43:10 +0200

    Update App.jsx

--- Client Side/src/App.jsx
+++ Client Side/src/App.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./globals.css";
-import { Toaster } from "react-hot-toast";
-import AuthLayout from "./Components/_auth/AuthLayout.jsx";
-import RootLayout from "./Components/_root/RootLayout.jsx";
+import SignupForm from "./Components/Forms/SignupForm.jsx";
+import SigninForm from "./Components/Forms/SigninForm.jsx";
 import {
   Home,
   AllUsers,
@@ -14,52 +12,35 @@ import {
   Saves,
   PostDetails,
   Profile,
-  SignupForm,
-  SigninForm,
 } from "./pages/index.js";
-
+import AuthLayout from "./Components/_auth/AuthLayout.jsx";
+import RootLayout from "./Components/_root/RootLayout.jsx";
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <main>
       <div>
         <Toaster />
       </div>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <RootLayout>
-                <Route index element={<Home />} />
-                <Route path="explore" element={<Explore />} />
-                <Route path="saved" element={<Saves />} />
-                <Route path="all-users" element={<AllUsers />} />
-                <Route path="create-post" element={<CreatePost />} />
-                <Route path="update-post/:id" element={<EditPost />} />
-                <Route path="posts/:id" element={<PostDetails />} />
-                <Route path="profile/:id/*" element={<Profile />} />
-                <Route path="update-profile/:id" element={<EditProfile />} />
-              </RootLayout>
-            }
-          />
-          <Route
-            path="sign-in"
-            element={
-              <AuthLayout>
-                <Route index element={<SigninForm />} />
-              </AuthLayout>
-            }
-          />
-          <Route
-            path="sign-up"
-            element={
-              <AuthLayout>
-                <Route index element={<SignupForm />} />
-              </AuthLayout>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
+      
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saves />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<EditProfile />} />
+        </Route>
+      </Routes>
     </main>
   );
 }
