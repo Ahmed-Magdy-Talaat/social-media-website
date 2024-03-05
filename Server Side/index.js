41cc1adc8f86c7863b87bbdb35751d03b638991e
Ahmed-Magdy-Talaat <85371544+Ahmed-Magdy-Talaat@users.noreply.github.com>
2024-03-05 20:10:24 +0200

    Update index.js

--- Server Side/index.js
+++ Server Side/index.js
@@ -44,7 +44,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-export const Port = process.env.PORT || 3000;
+const Port = process.env.PORT || 3000;
 
 /* mongoose */
 mongoose
